16a2e7109ef8468011a9d5b1e686b780b0d50a12
mignot <andreas.garcia@bodzify.com>
2025-10-19 23:37:53 +0200

    test: move has_id3v1_header to ID3v1 directory
    - Created ID3v1HeaderVerifier class in id3v1/id3v1_header_verifier.py
    - Moved has_id3v1_header method from ID3v2 to ID3v1 directory for better organization
    - Updated ID3v1 __init__.py to export new class
    - Removed non-existent id3v1_tool import to fix Pylance error

--- audiometa/test/helpers/id3v1/__init__.py
+++ audiometa/test/helpers/id3v1/__init__.py
@@ -1,11 +1,11 @@
 """ID3v1 metadata format helpers."""
 
-from .id3v1_tool import Id3v1Tool
 from .id3v1_metadata_deleter import ID3v1MetadataDeleter
 from .id3v1_metadata_setter import ID3v1MetadataSetter
+from .id3v1_header_verifier import ID3v1HeaderVerifier
 
 __all__ = [
-    "Id3v1Tool",
     "ID3v1MetadataDeleter",
-    "ID3v1MetadataSetter"
+    "ID3v1MetadataSetter",
+    "ID3v1HeaderVerifier"
 ]
--- audiometa/test/helpers/id3v2/id3v2_header_verifier.py
+++ audiometa/test/helpers/id3v2/id3v2_header_verifier.py
@@ -15,14 +15,3 @@ class ID3HeaderVerifier:
                 return header[:3] == b'ID3'
         except (IOError, OSError):
             return False
-    
-    @staticmethod
-    def has_id3v1_header(file_path: Path) -> bool:
-        """Check if file has ID3v1 header by reading the last 128 bytes."""
-        try:
-            with open(file_path, 'rb') as f:
-                f.seek(-128, 2)  # Seek to last 128 bytes
-                header = f.read(128)
-                return header[:3] == b'TAG'
-        except (IOError, OSError):
-            return False
