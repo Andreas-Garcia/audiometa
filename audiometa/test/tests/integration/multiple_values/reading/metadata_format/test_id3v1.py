82abe2f27ad93cf37600ed4a47f7a8c297935d8e
mignot <andreas.garcia@bodzify.com>
2025-10-20 12:11:33 +0200

    test: refactor ID3v1 artist setting to use dedicated setter method

--- audiometa/test/tests/integration/multiple_values/reading/metadata_format/test_id3v1.py
+++ audiometa/test/tests/integration/multiple_values/reading/metadata_format/test_id3v1.py
@@ -2,12 +2,13 @@
 from audiometa import get_specific_metadata
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
+from test.helpers.id3v1.id3v1_metadata_setter import ID3v1MetadataSetter
 
 
 class TestId3v1:
     def test_semicolon_separated_artists(self):
         with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
-            test_file.set_id3v1_artist("Artist One;Artist Two")
+            ID3v1MetadataSetter.set_artist("Artist One;Artist Two")
             
             artists = get_specific_metadata(test_file.path, UnifiedMetadataKey.ARTISTS_NAMES, metadata_format="ID3v1")
             
