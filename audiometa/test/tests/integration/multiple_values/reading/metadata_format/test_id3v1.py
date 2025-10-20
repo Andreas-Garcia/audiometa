749fb46f176ba684ab77ee99e8a322f071a484b2
mignot <andreas.garcia@bodzify.com>
2025-10-20 11:20:00 +0200

    test: update test_id3v1.py to use get_specific_metadata
    - Changed from get_merged_unified_metadata to get_specific_metadata
    - Simplified the test by directly getting artists from specific metadata format
    - Completed the incomplete line in the test method

--- audiometa/test/tests/integration/multiple_values/reading/metadata_format/test_id3v1.py
+++ audiometa/test/tests/integration/multiple_values/reading/metadata_format/test_id3v1.py
@@ -1,6 +1,6 @@
 import pytest
 
-from audiometa import get_merged_unified_metadata
+from audiometa import get_specific_metadata
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
@@ -10,8 +10,7 @@ class TestId3v1:
         with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
             test_file.set_id3v1_artist("Artist One;Artist Two")
             
-            unified_metadata = get_merged_unified_metadata(test_file.path)
-            artists = unified_metadata.get(UnifiedMetadataKey.ARTISTS_NAMES)
+            artists = get_specific_metadata(test_file.path, UnifiedMetadataKey.ARTISTS_NAMES, metadata_format="ID3v1")
             
             assert isinstance(artists, list)
             assert len(artists) == 2
