10adafbaa57d89a4bbfd08fa4303aa642a6a96d8
mignot <andreas.garcia@bodzify.com>
2025-10-23 01:25:34 +0200

    test: refactor FLAC writing tests to use context manager for temporary files

--- audiometa/test/tests/integration/audio_format/flac/test_flac_writing.py
+++ audiometa/test/tests/integration/audio_format/flac/test_flac_writing.py
@@ -4,30 +4,35 @@ from audiometa import update_metadata
 from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.vorbis.vorbis_metadata_getter import VorbisMetadataGetter
+from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
 
 @pytest.mark.integration
 class TestFlacWriting:
-    def test_vorbis_metadata_writing_flac(self, temp_flac_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title Vorbis"}
-        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.VORBIS)
-        title = VorbisMetadataGetter.get_title(temp_flac_file)
-        assert title == "Test Title Vorbis"
+    def test_vorbis_metadata_writing_flac(self):
+        with TempFileWithMetadata({}, "flac") as temp_flac_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title Vorbis"}
+            update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.VORBIS)
+            title = VorbisMetadataGetter.get_title(temp_flac_file)
+            assert title == "Test Title Vorbis"
 
-    def test_id3v2_3_metadata_writing_flac(self, temp_flac_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.3"}
-        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
-        title = VorbisMetadataGetter.get_title(temp_flac_file)
+    def test_id3v2_3_metadata_writing_flac(self):
+        with TempFileWithMetadata({}, "flac") as temp_flac_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.3"}
+            update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
+            title = VorbisMetadataGetter.get_title(temp_flac_file)
         assert title == "Test Title ID3v2.3"
 
-    def test_id3v2_4_metadata_writing_flac(self, temp_flac_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.4"}
-        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
+    def test_id3v2_4_metadata_writing_flac(self):
+        with TempFileWithMetadata({}, "flac") as temp_flac_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.4"}
+            update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
         title = VorbisMetadataGetter.get_title(temp_flac_file)
         assert title == "Test Title ID3v2.4"
 
-    def test_id3v1_metadata_writing_flac(self, temp_flac_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v1"}
+    def test_id3v1_metadata_writing_flac(self):
+        with TempFileWithMetadata({}, "flac") as temp_flac_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v1"}
         update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V1)
         title = VorbisMetadataGetter.get_title(temp_flac_file)
         assert title == "Test Title ID3v1"
