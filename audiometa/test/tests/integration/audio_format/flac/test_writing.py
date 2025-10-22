36a6369598832b495988f9a5bb2a9bfa9a90aee4
mignot <andreas.garcia@bodzify.com>
2025-10-22 23:50:28 +0200

    test: update ID3v2 metadata writing tests for FLAC files to specify versions

--- audiometa/test/tests/integration/audio_format/flac/test_writing.py
+++ audiometa/test/tests/integration/audio_format/flac/test_writing.py
@@ -12,12 +12,18 @@ class TestFlacWriting:
         read_metadata = get_unified_metadata(temp_flac_file, metadata_format=MetadataFormat.VORBIS)
         assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title Vorbis"
 
-    def test_id3v2_metadata_writing_flac(self, temp_flac_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2"}
-        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2)
+    def test_id3v2_3_metadata_writing_flac(self, temp_flac_file):
+        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.3"}
+        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
         read_metadata = get_unified_metadata(temp_flac_file, metadata_format=MetadataFormat.ID3V2)
-        assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2"
-        
+        assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2.3"
+
+    def test_id3v2_4_metadata_writing_flac(self, temp_flac_file):
+        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.4"}
+        update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
+        read_metadata = get_unified_metadata(temp_flac_file, metadata_format=MetadataFormat.ID3V2)
+        assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2.4"
+
     def test_id3v1_metadata_writing_flac(self, temp_flac_file):
         metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v1"}
         update_metadata(temp_flac_file, metadata, metadata_format=MetadataFormat.ID3V1)
