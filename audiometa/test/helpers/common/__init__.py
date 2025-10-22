cf25e3dbe9a06df082476d846c0fd5b3f61b5e08
mignot <andreas.garcia@bodzify.com>
2025-10-23 01:36:24 +0200

    test: remove ComprehensiveHeaderVerifier from common utilities

--- audiometa/test/helpers/common/__init__.py
+++ audiometa/test/helpers/common/__init__.py
@@ -1,12 +1,10 @@
 """Common utilities for metadata testing."""
 
 from .audio_file_creator import AudioFileCreator
-from .comprehensive_header_verifier import ComprehensiveHeaderVerifier
 from .external_tool_runner import run_external_tool, run_script
 
 __all__ = [
     "AudioFileCreator",
-    "ComprehensiveHeaderVerifier", 
     "run_external_tool",
     "run_script"
 ]
