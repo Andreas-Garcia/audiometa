00739c3a35b0782f055933ec9a7fb168794c2363
mignot <andreas.garcia@bodzify.com>
2025-10-22 18:52:31 +0200

    test: enhance genre entry validation by checking for null separators in raw metadata

--- audiometa/test/tests/integration/specific_metadata/genre/reading/test_reading_strategies.py
+++ audiometa/test/tests/integration/specific_metadata/genre/reading/test_reading_strategies.py
@@ -59,7 +59,14 @@ class TestID3v2GenreReadingStrategies:
             ID3v2MetadataSetter.set_genres(test_file.path, ["(17)Rock(6)Blues"], version="2.4")
             
             # Validate raw metadata
-            assert "(17)Rock(6)Blues" in ID3v2MetadataGetter.get_raw_metadata(test_file.path)
+            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
+            
+            assert "(17)Rock(6)Blues" in raw_metadata
+            
+            # assert no null separators in raw metadata genre
+            assert '\x00' not in raw_metadata
+            
+            
             
             # Read via API
             genres = get_specific_metadata(test_file.path, UnifiedMetadataKey.GENRES_NAMES)
