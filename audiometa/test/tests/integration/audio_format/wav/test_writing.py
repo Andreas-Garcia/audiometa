cd095ab63dd5e6a79939c536ea7021bc54b0c450
mignot <andreas.garcia@bodzify.com>
2025-10-22 23:59:05 +0200

    test: add ID3v2.3 and ID3v2.4 metadata writing tests for WAV files

--- audiometa/test/tests/integration/audio_format/wav/test_writing.py
+++ audiometa/test/tests/integration/audio_format/wav/test_writing.py
@@ -2,6 +2,7 @@ import pytest
 from audiometa import get_unified_metadata, update_metadata
 from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
+from test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
 
 @pytest.mark.integration
@@ -12,8 +13,16 @@ class TestWavWriting:
         read_metadata = get_unified_metadata(temp_wav_file, metadata_format=MetadataFormat.RIFF)
         assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title RIFF"
 
-    def test_id3v2_metadata_writing_wav(self, temp_wav_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2"}
-        update_metadata(temp_wav_file, metadata, metadata_format=MetadataFormat.ID3V2)
-        read_metadata = get_unified_metadata(temp_wav_file, metadata_format=MetadataFormat.ID3V2)
-        assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2"
+    def test_id3v2_3_metadata_writing_wav(self):
+        with TempFileWithMetadata({}, "id3v2.3") as test_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.3"}
+            update_metadata(test_file.path, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
+            read_metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2)
+            assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2.3"
+            
+    def test_id3v2_4_metadata_writing_wav(self):
+        with TempFileWithMetadata({}, "id3v2.4") as test_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.4"}
+            update_metadata(test_file.path, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
+            read_metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2)
+            assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v2.4"
