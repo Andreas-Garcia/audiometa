4a6fae593afeb03063e9add598e8123c664af8d1
mignot <andreas.garcia@bodzify.com>
2025-10-22 23:49:53 +0200

    test: refactor ID3v2 metadata reading tests for FLAC files

--- audiometa/test/tests/integration/audio_format/flac/test_reading.py
+++ audiometa/test/tests/integration/audio_format/flac/test_reading.py
@@ -2,6 +2,7 @@ import pytest
 from audiometa import get_unified_metadata
 from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
+from test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
 
 @pytest.mark.integration
@@ -13,12 +14,27 @@ class TestFlacReading:
         # Assuming 'a' * 30
         assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
 
-    def test_id3v2_metadata_reading_flac(self, metadata_id3v2_small_flac):
-        metadata = get_unified_metadata(metadata_id3v2_small_flac, metadata_format=MetadataFormat.ID3V2)
-        assert isinstance(metadata, dict)
-        assert UnifiedMetadataKey.TITLE in metadata
-        # Assuming 'a' * 30
-        assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
+    def test_id3v2_3_metadata_reading_flac(self):
+        with TempFileWithMetadata({}, "id3v2.3") as test_file:
+            # Set test metadata
+            from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
+            ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.3 Long Title That Exceeds ID3v1 Limits")
+
+            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version="2.3")
+            assert isinstance(metadata, dict)
+            assert UnifiedMetadataKey.TITLE in metadata
+            assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.3 Long Title That Exceeds ID3v1 Limits"
+            
+    def test_id3v2_4_metadata_reading_flac(self):
+        with TempFileWithMetadata({}, "id3v2.4") as test_file:
+            # Set test metadata
+            from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
+            ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.4 Long Title That Exceeds ID3v1 Limits")
+
+            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version="2.4")
+            assert isinstance(metadata, dict)
+            assert UnifiedMetadataKey.TITLE in metadata
+            assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.4 Long Title That Exceeds ID3v1 Limits"
 
     def test_id3v1_metadata_reading_flac(self, metadata_id3v1_small_flac):
         metadata = get_unified_metadata(metadata_id3v1_small_flac, metadata_format=MetadataFormat.ID3V1)
