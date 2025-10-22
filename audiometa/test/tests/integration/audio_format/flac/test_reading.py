b6d2a15d739f32a6d4c04c26c476cbc369de7ed4
mignot <andreas.garcia@bodzify.com>
2025-10-22 23:42:24 +0200

    test: add ID3v1 metadata reading test for FLAC files

--- audiometa/test/tests/integration/audio_format/flac/test_reading.py
+++ audiometa/test/tests/integration/audio_format/flac/test_reading.py
@@ -19,3 +19,9 @@ class TestFlacReading:
         assert UnifiedMetadataKey.TITLE in metadata
         # Assuming 'a' * 30
         assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
+
+    def test_id3v1_metadata_reading_flac(self, metadata_id3v1_small_flac):
+        metadata = get_unified_metadata(metadata_id3v1_small_flac, metadata_format=MetadataFormat.ID3V1)
+        assert isinstance(metadata, dict)
+        assert UnifiedMetadataKey.TITLE in metadata
+        assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
