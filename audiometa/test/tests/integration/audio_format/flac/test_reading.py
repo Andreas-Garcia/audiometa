36aa5fc4422baaba418779655e0313ec583c9f5c
mignot <andreas.garcia@bodzify.com>
2025-10-22 23:53:00 +0200

    test: update ID3v2 metadata reading tests for FLAC files to use version lists

--- audiometa/test/tests/integration/audio_format/flac/test_reading.py
+++ audiometa/test/tests/integration/audio_format/flac/test_reading.py
@@ -20,7 +20,7 @@ class TestFlacReading:
             from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
             ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.3 Long Title That Exceeds ID3v1 Limits")
 
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version="2.3")
+            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version=[2, 3, 0])
             assert isinstance(metadata, dict)
             assert UnifiedMetadataKey.TITLE in metadata
             assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.3 Long Title That Exceeds ID3v1 Limits"
@@ -31,7 +31,7 @@ class TestFlacReading:
             from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
             ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.4 Long Title That Exceeds ID3v1 Limits")
 
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version="2.4")
+            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, version=[2, 4, 0])
             assert isinstance(metadata, dict)
             assert UnifiedMetadataKey.TITLE in metadata
             assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.4 Long Title That Exceeds ID3v1 Limits"
