a116cf8be25a3ab863fb21661eaf64e46de495e3
mignot <andreas.garcia@bodzify.com>
2025-10-23 16:24:03 +0200

    test: standardize parameter names in ID3v2 metadata update and retrieval tests

--- audiometa/test/tests/integration/audio_format/wav/test_wav_writing.py
+++ audiometa/test/tests/integration/audio_format/wav/test_wav_writing.py
@@ -21,9 +21,9 @@ class TestWavWriting:
     def test_id3v2_3_metadata_writing_wav(self):
         with TempFileWithMetadata({}, "id3v2.3") as test_file:
             metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.3"}
-            update_metadata(test_file.path, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
-            
-            title = ID3v2MetadataGetter.get_title(test_file.path, version=(2, 3))
+            update_metadata(test_file.path, unified_metadata=metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
+                        
+            title = ID3v2MetadataGetter.get_title(test_file.path, version=3)
             assert title == "Test Title ID3v2.3"
             
     def test_id3v2_4_metadata_writing_wav(self):
@@ -31,5 +31,5 @@ class TestWavWriting:
             metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v2.4"}
             update_metadata(test_file.path, metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
             
-            title = ID3v2MetadataGetter.get_title(test_file.path, version=(2, 4))
+            title = ID3v2MetadataGetter.get_title(test_file.path, version=4)
             assert title == "Test Title ID3v2.4"
