839eab55fac484b2f3256b12fa607125b39a45a8
mignot <andreas.garcia@bodzify.com>
2025-10-24 14:28:20 +0200

    refactor(test): replace get_unified_metadata with get_unified_metadata_field for improved clarity in FLAC reading tests

--- audiometa/test/tests/integration/audio_format/flac/test_flac_reading.py
+++ audiometa/test/tests/integration/audio_format/flac/test_flac_reading.py
@@ -1,10 +1,10 @@
 import pytest
-from audiometa import get_unified_metadata
-from audiometa.test.helpers.id3v1.id3v1_metadata_getter import ID3v1MetadataGetter
-from audiometa.test.helpers.vorbis.vorbis_metadata_setter import VorbisMetadataSetter
+from audiometa import get_unified_metadata_field
 from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
+from audiometa.test.helpers.id3v1.id3v1_metadata_getter import ID3v1MetadataGetter
+from audiometa.test.helpers.id3v1.id3v1_metadata_setter import ID3v1MetadataSetter
 
 
 @pytest.mark.integration
@@ -15,11 +15,8 @@ class TestFlacReading:
             from audiometa.test.helpers.vorbis import VorbisMetadataSetter
             VorbisMetadataSetter.add_title(test_file.path, 'a' * 30)
 
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.VORBIS)
-            assert isinstance(metadata, dict)
-            assert UnifiedMetadataKey.TITLE in metadata
-            # Assuming 'a' * 30
-            assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
+            title = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.TITLE, metadata_format=MetadataFormat.VORBIS)
+            assert title == 'a' * 30
 
     def test_id3v2_3_metadata_reading_flac(self):
         with TempFileWithMetadata({}, "id3v2.3") as test_file:
@@ -27,10 +24,8 @@ class TestFlacReading:
             from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
             ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.3 Long Title That Exceeds ID3v1 Limits")
 
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
-            assert isinstance(metadata, dict)
-            assert UnifiedMetadataKey.TITLE in metadata
-            assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.3 Long Title That Exceeds ID3v1 Limits"
+            title = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.TITLE, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
+            assert title == "ID3v2.3 Long Title That Exceeds ID3v1 Limits"
             
     def test_id3v2_4_metadata_reading_flac(self):
         with TempFileWithMetadata({}, "id3v2.4") as test_file:
@@ -38,18 +33,13 @@ class TestFlacReading:
             from audiometa.test.helpers.id3v2 import ID3v2MetadataSetter
             ID3v2MetadataSetter.set_title(test_file.path, "ID3v2.4 Long Title That Exceeds ID3v1 Limits")
 
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
-            assert isinstance(metadata, dict)
-            assert UnifiedMetadataKey.TITLE in metadata
-            assert metadata[UnifiedMetadataKey.TITLE] == "ID3v2.4 Long Title That Exceeds ID3v1 Limits"
+            title = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.TITLE, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
+            assert title == "ID3v2.4 Long Title That Exceeds ID3v1 Limits"
 
     def test_id3v1_metadata_reading_flac(self, metadata_id3v1_small_flac):
         with TempFileWithMetadata({}, "flac") as test_file:
-            from audiometa.test.helpers.id3v1.id3v1_metadata_setter import ID3v1MetadataSetter
             ID3v1MetadataSetter.set_title(test_file.path, 'a' * 30)
             assert ID3v1MetadataGetter.get_title(test_file.path) == 'a' * 30
             
-            metadata = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V1)
-            assert isinstance(metadata, dict)
-            assert UnifiedMetadataKey.TITLE in metadata
-            assert metadata[UnifiedMetadataKey.TITLE] == 'a' * 30
+            title = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.TITLE, metadata_format=MetadataFormat.ID3V1)
+            assert title == 'a' * 30
