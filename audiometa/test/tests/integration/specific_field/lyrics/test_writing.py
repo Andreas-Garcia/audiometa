672defe1d6a2f291618579e6b1ea4ea3076877eb
mignot <andreas.garcia@bodzify.com>
2025-10-25 10:46:22 +0200

    test: add ID3v2 version-specific tests for lyrics writing

--- audiometa/test/tests/integration/specific_field/lyrics/test_writing.py
+++ audiometa/test/tests/integration/specific_field/lyrics/test_writing.py
@@ -3,23 +3,30 @@ import pytest
 from audiometa import update_metadata
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.utils.MetadataFormat import MetadataFormat
-from audiometa.exceptions import MetadataNotSupportedError
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 from audiometa.test.helpers.id3v2.id3v2_metadata_getter import ID3v2MetadataGetter
 from audiometa.test.helpers.riff.riff_metadata_getter import RIFFMetadataGetter
-from audiometa.test.helpers.riff.riff_metadata_setter import RIFFMetadataSetter
 
 @pytest.mark.integration
 class TestLyricsWriting:
-    def test_id3v2(self):
+    def test_id3v2_3(self):
         with TempFileWithMetadata({}, "mp3") as test_file:
             test_lyrics = "These are test lyrics\nWith multiple lines\nFor testing purposes"
             test_metadata = {UnifiedMetadataKey.LYRICS: test_lyrics}
-            update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.ID3V2)
+            update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
             
-            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
+            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, version='2.3')
             assert f"USLT={test_lyrics}" in raw_metadata
 
+    def test_id3v2_4(self):
+        with TempFileWithMetadata({}, "mp3") as test_file:
+            test_lyrics = "These are test lyrics\nWith multiple lines\nFor testing purposes"
+            test_metadata = {UnifiedMetadataKey.LYRICS: test_lyrics}
+            update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 4, 0))
+            
+            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, version='2.4')
+            assert f"USLT={test_lyrics}" in raw_metadata
+            
     def test_riff(self):
         with TempFileWithMetadata({}, "wav") as test_file:
             test_lyrics = "RIFF test lyrics\nWith multiple lines\nFor testing purposes"
