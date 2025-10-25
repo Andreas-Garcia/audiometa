1e13125e012f7355c0693cbae84579049c746fb0
mignot <andreas.garcia@bodzify.com>
2025-10-25 13:45:29 +0200

    test: add integration tests for unsupported metadata fields across formats

--- audiometa/test/tests/integration/specific_field/field_not_supported/test_reading.py
+++ audiometa/test/tests/integration/specific_field/field_not_supported/test_reading.py
@@ -4,28 +4,33 @@ import pytest
 
 from audiometa import get_unified_metadata_field
 from audiometa.exceptions import MetadataFieldNotSupportedByLib
-from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
+from audiometa.utils.MetadataFormat import MetadataFormat
 
 
 @pytest.mark.integration
 class TestFieldNotSupportedReading:
+    def test_field_not_supported_all_formats(self):
+        with TempFileWithMetadata({}, "mp3") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
+                get_unified_metadata_field(test_file.path, 'FIELD_NOT_SUPPORTED')
+
     def test_id3v1(self):
         with TempFileWithMetadata({"title": "Test Song"}, "id3v1") as test_file:
             with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
-                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+                get_unified_metadata_field(test_file.path, 'FIELD_NOT_SUPPORTED', metadata_format=MetadataFormat.ID3V1)
 
     def test_id3v2(self):
         with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
             with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
-                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+                get_unified_metadata_field(test_file.path, 'FIELD_NOT_SUPPORTED', metadata_format=MetadataFormat.ID3V2)
 
     def test_vorbis(self):
         with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
             with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
-                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+                get_unified_metadata_field(test_file.path, 'FIELD_NOT_SUPPORTED', metadata_format=MetadataFormat.VORBIS)
 
     def test_riff(self):
         with TempFileWithMetadata({"title": "Test Song"}, "wav") as test_file:
             with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
-                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+                get_unified_metadata_field(test_file.path, 'FIELD_NOT_SUPPORTED', metadata_format=MetadataFormat.RIFF)
