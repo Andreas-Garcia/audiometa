edf5079b692b774fae20fb8d5a6b1ad370c4cc2b
mignot <andreas.garcia@bodzify.com>
2025-10-25 15:22:22 +0200

    test: update BPM reading tests for ID3v1, ID3v2, Vorbis, and RIFF formats

--- audiometa/test/tests/integration/specific_field/bpm/test_reading.py
+++ audiometa/test/tests/integration/specific_field/bpm/test_reading.py
@@ -3,6 +3,9 @@
 import pytest
 
 from audiometa import get_unified_metadata_field
+from audiometa.exceptions import MetadataFieldNotSupportedByMetadataFormatError
+from audiometa.test.helpers.id3v2.id3v2_metadata_setter import ID3v2MetadataSetter
+from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
@@ -11,23 +14,20 @@ from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 class TestBpmReading:
     def test_id3v1(self):
         with TempFileWithMetadata({"title": "Test Song"}, "id3v1") as test_file:
-            test_file.set_id3v1_max_metadata()
-            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM)
-            assert bpm is None
+            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM, metadata_format=MetadataFormat.ID3V1)
 
     def test_id3v2(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
-            test_file.set_id3v2_max_metadata()
-            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM)
+        with TempFileWithMetadata({"title": "Test Song", "bpm": 999}, "id3v2.4") as test_file:
+            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM, metadata_format=MetadataFormat.ID3V2)
             assert bpm == 999
 
     def test_vorbis(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
-            test_file.set_vorbis_max_metadata()
-            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM)
+        with TempFileWithMetadata({"title": "Test Song", "bpm": 999}, "flac") as test_file:
+            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM, metadata_format=MetadataFormat.VORBIS)
             assert bpm == 999
 
     def test_riff(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "wav") as test_file:
-            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM)
-            assert bpm is None
+        with TempFileWithMetadata({"title": "Test Song", "bpm": 999}, "wav") as test_file:
+            bpm = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.BPM, metadata_format=MetadataFormat.RIFF)
+            assert bpm == 999
