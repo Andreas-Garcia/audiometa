1c5513229b93bf19e60b6e487f5ee5e5d5d7aa25
mignot <andreas.garcia@bodzify.com>
2025-10-25 20:08:25 +0200

    test: enhance Vorbis lyrics deletion tests to validate metadata handling

--- audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
+++ audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
@@ -6,6 +6,7 @@ from audiometa.exceptions import MetadataFieldNotSupportedByMetadataFormatError
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 from audiometa.test.helpers.id3v2.id3v2_metadata_getter import ID3v2MetadataGetter
 from audiometa.test.helpers.id3v2.id3v2_metadata_setter import ID3v2MetadataSetter
+from audiometa.test.helpers.vorbis.vorbis_metadata_getter import VorbisMetadataGetter
 from audiometa.utils.MetadataFormat import MetadataFormat
 
 
@@ -46,9 +47,15 @@ class TestLyricsDeleting:
 
     def test_delete_lyrics_vorbis(self):        
         with TempFileWithMetadata({}, "flac") as test_file:
-            # Vorbis doesn't support lyrics in this implementation, so writing should fail
-            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError, match="UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS metadata not supported by this format"):
-                update_metadata(test_file.path, {UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS: "Test lyrics"}, metadata_format=MetadataFormat.VORBIS)
+            from audiometa.test.helpers.vorbis.vorbis_metadata_setter import VorbisMetadataSetter
+            VorbisMetadataSetter.set_lyrics(test_file.path, "Test lyrics")
+            raw_metadata = VorbisMetadataGetter.get_raw_metadata(test_file.path)
+            assert "LYRICS=Test lyrics" in raw_metadata
+        
+            update_metadata(test_file.path, {UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS: None}, metadata_format=MetadataFormat.VORBIS)
+            
+            raw_metadata = VorbisMetadataGetter.get_raw_metadata(test_file.path)
+            assert "LYRICS=" not in raw_metadata
 
     def test_delete_lyrics_preserves_other_fields(self):
         with TempFileWithMetadata({}, "mp3") as test_file:
@@ -66,20 +73,20 @@ class TestLyricsDeleting:
     def test_delete_lyrics_already_none(self):
         with TempFileWithMetadata({}, "flac") as test_file:
             # Try to delete lyrics that don't exist
-            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
-            assert "UNSYNCHRONIZED_LYRICS=" not in raw_metadata
+            raw_metadata = VorbisMetadataGetter.get_raw_metadata(test_file.path)
+            assert "LYRICS=" not in raw_metadata
             
             update_metadata(test_file.path, {UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS: None}, metadata_format=MetadataFormat.VORBIS)
 
-            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
-            assert "UNSYNCHRONIZED_LYRICS=" not in raw_metadata
+            raw_metadata = VorbisMetadataGetter.get_raw_metadata(test_file.path)
+            assert "LYRICS=" not in raw_metadata
             
     def test_delete_lyrics_empty_string(self):
         with TempFileWithMetadata({}, "mp3") as test_file:
-            ID3v2MetadataSetter.set_lyrics("")
+            ID3v2MetadataSetter.set_lyrics(test_file.path, " ")
             raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
-            assert "USLT=" in raw_metadata  # Empty lyrics field exists
+            assert 'USLT' in raw_metadata  # Lyrics field exists
             
             update_metadata(test_file.path, {UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS: None}, metadata_format=MetadataFormat.ID3V2)
             raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
-            assert "USLT=" not in raw_metadata
+            assert raw_metadata is None or 'USLT' not in raw_metadata
