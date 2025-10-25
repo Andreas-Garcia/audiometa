8d33c73e44ad48a8942a7e6f1dadb84073c82d62
mignot <andreas.garcia@bodzify.com>
2025-10-25 15:37:00 +0200

    test: update copyright reading test for ID3v1 to check for unsupported metadata exception

--- audiometa/test/tests/integration/specific_field/copyright/test_reading.py
+++ audiometa/test/tests/integration/specific_field/copyright/test_reading.py
@@ -3,6 +3,8 @@
 import pytest
 
 from audiometa import get_unified_metadata_field
+from audiometa.exceptions import MetadataFieldNotSupportedByMetadataFormatError
+from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 
@@ -11,9 +13,8 @@ from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
 class TestCopyrightReading:
     def test_id3v1(self):
         with TempFileWithMetadata({"title": "Test Song"}, "id3v1") as test_file:
-            test_file.set_id3v1_max_metadata()
-            copyright_info = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
-            assert copyright_info is None
+            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError, match="UnifiedMetadataKey.COPYRIGHT metadata not supported by this format"):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT, metadata_format=MetadataFormat.ID3V1)
 
     def test_id3v2(self):
         with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
