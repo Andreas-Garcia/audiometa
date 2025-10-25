0285b0080066818d3a972ecfb64947598ed816c8
mignot <andreas.garcia@bodzify.com>
2025-10-25 18:22:42 +0200

    test: add test for deleting composer metadata in ID3v1 format and update ID3v2 assertion

--- audiometa/test/tests/integration/specific_field/composer/test_deleting.py
+++ audiometa/test/tests/integration/specific_field/composer/test_deleting.py
@@ -13,19 +13,18 @@ from audiometa.utils.MetadataFormat import MetadataFormat
 
 @pytest.mark.integration
 class TestComposerDeleting:
+    def test_delete_composer_id3v1(self):
+        with TempFileWithMetadata({}, "mp3") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError, match="UnifiedMetadataKey.COMPOSERS metadata not supported by this format"):
+                update_metadata(test_file.path, {UnifiedMetadataKey.COMPOSERS: ["Test Composer"]}, metadata_format=MetadataFormat.ID3V1)
+    
     def test_delete_composer_id3v2(self):
         with TempFileWithMetadata({"composer": "Test Composer"}, "id3v2.4") as test_file:
-            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, "2.4")
-            assert "TCOM=Test Composer" in raw_metadata
+            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, version="2.4")
+            assert raw_metadata['TCOM'] == ["Test Composer"]
         
             update_metadata(test_file.path, {UnifiedMetadataKey.COMPOSERS: None}, metadata_format=MetadataFormat.ID3V2)
             assert get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COMPOSERS) is None
-
-    def test_delete_composer_id3v1(self):
-        with TempFileWithMetadata({}, "mp3") as test_file:
-            # ID3v1 format raises exception for unsupported metadata
-            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError, match="UnifiedMetadataKey.COMPOSERS metadata not supported by this format"):
-                update_metadata(test_file.path, {UnifiedMetadataKey.COMPOSERS: ["Test Composer"]}, metadata_format=MetadataFormat.ID3V1)
 
     def test_delete_composer_riff(self):
         with TempFileWithMetadata({}, "wav") as test_file:
