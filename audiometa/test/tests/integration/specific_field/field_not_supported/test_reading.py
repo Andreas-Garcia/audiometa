20229a243315f9f406ca4e8cde186d7a0ebfd25c
mignot <andreas.garcia@bodzify.com>
2025-10-25 12:15:27 +0200

    test: add integration tests for reading unsupported COPYRIGHT metadata field in ID3v1, ID3v2, Vorbis, and RIFF formats

---
+++ audiometa/test/tests/integration/specific_field/field_not_supported/test_reading.py
@@ -0,0 +1,31 @@
+
+
+import pytest
+
+from audiometa import get_unified_metadata_field
+from audiometa.exceptions import MetadataFieldNotSupportedByLib
+from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
+from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
+
+
+@pytest.mark.integration
+class TestCopyrightReading:
+    def test_id3v1(self):
+        with TempFileWithMetadata({"title": "Test Song"}, "id3v1") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+
+    def test_id3v2(self):
+        with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+
+    def test_vorbis(self):
+        with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
+
+    def test_riff(self):
+        with TempFileWithMetadata({"title": "Test Song"}, "wav") as test_file:
+            with pytest.raises(MetadataFieldNotSupportedByLib, match="FIELD_NOT_SUPPORTED metadata not supported by the library."):
+                get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
