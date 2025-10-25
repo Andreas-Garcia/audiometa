bb6dfd8324a350b61dae84751546512ec9e7e5b7
mignot <andreas.garcia@bodzify.com>
2025-10-25 15:43:08 +0200

    test: update copyright reading tests to verify correct handling of copyright metadata in ID3v2 and Vorbis formats

--- audiometa/test/tests/integration/specific_field/copyright/test_reading.py
+++ audiometa/test/tests/integration/specific_field/copyright/test_reading.py
@@ -17,16 +17,15 @@ class TestCopyrightReading:
                 get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT, metadata_format=MetadataFormat.ID3V1)
 
     def test_id3v2(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "mp3") as test_file:
-            test_file.set_id3v2_max_metadata()
+        with TempFileWithMetadata({"title": "Test Song", "copyright": "Test Copyright"}, "mp3") as test_file:
             copyright_info = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
-            assert copyright_info == "a" * 1000
+            assert copyright_info == "Test Copyright"
 
     def test_vorbis(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
+        with TempFileWithMetadata({"title": "Test Song", "copyright": "Test Copyright"}, "flac") as test_file:
             test_file.set_vorbis_max_metadata()
             copyright_info = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.COPYRIGHT)
-            assert copyright_info == "a" * 1000
+            assert copyright_info == "Test Copyright"
 
     def test_riff(self):
         with TempFileWithMetadata({"title": "Test Song"}, "wav") as test_file:
